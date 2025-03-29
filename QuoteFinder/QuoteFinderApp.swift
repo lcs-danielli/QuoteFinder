e5b5a6bef1d52ab2d4b84f9b8297a7c0792ca2d7
Daniel Li <danli@lcs.on.ca>
2025-03-28 20:20:46 -0400

    Initial Commit

---
+++ QuoteFinder/QuoteFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteFinderApp.swift
+//  QuoteFinder
+//
+//  Created by 李泽宇 on 2025-03-28.
+//
+
+import SwiftUI
+
+@main
+struct QuoteFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
